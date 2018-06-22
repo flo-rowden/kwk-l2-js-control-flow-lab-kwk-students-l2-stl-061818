80052d4b7bc3aa01f402fa802980aeadaf0497c3
Flora May Rowden <21frowden@nerinxhs.org>
2018-06-22 14:20:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function scuberGreetingForFeet(feet) {
-  if(feet<= 400) {
+  if(feet <= 400) {
     return 'This one is on me!';
     } else if (1999 , feet && feet , 2500) {
       return 'I will gladly take you thrty bucks.';
