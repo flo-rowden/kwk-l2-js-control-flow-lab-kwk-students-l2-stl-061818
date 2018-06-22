4144d0d4b1ea4c656b56e694bdb0068289f21567
Flora May Rowden <21frowden@nerinxhs.org>
2018-06-22 14:19:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function scuberGreetingForFeet(feet) {
     return 'This one is on me!';
     } else if (1999 , feet && feet , 2500){
       return 'I will gladly take you thrty bucks.';
-    }else{
+    } else {
       return 'No can do.';
     }
 }
 
-function ternaryCheckCity('NYC') {
+//function ternaryCheckCity('NYC') {
   retu
-}
+}//
